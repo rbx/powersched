dac28889ef74f3246d80ab9ae86f3bbf7afbaf18
Alexey Rybalchenko <alexryba@gmail.com>
2025-04-23 11:27:53 +0200

    make Weights a dataclass

--- weights.py
+++ weights.py
@@ -1,17 +1,15 @@
+from dataclasses import dataclass
 import numpy as np
 
+@dataclass
 class Weights:
-    def __init__(self, efficiency_weight, price_weight, idle_weight, job_age_weight):
-        self.efficiency_weight = efficiency_weight
-        self.price_weight = price_weight
-        self.idle_weight = idle_weight
-        self.job_age_weight = job_age_weight
+    efficiency_weight: float
+    price_weight: float
+    idle_weight: float
+    job_age_weight: float
 
     def __str__(self):
         return f"Weights: efficiency={self.efficiency_weight}, price={self.price_weight}, idle={self.idle_weight}, job_age={self.job_age_weight}. sum={self.sum():.2f}"
-
-    def __repr__(self):
-        return self.__str__()
 
     def sum(self):
         return round(np.sum([self.efficiency_weight, self.price_weight, self.idle_weight, self.job_age_weight]), 2)
